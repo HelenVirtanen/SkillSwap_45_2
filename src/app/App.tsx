994c7c65812e815e2c8b645a8ca4fa8f4770dac2
Roman Zhohov <zhohov.roman@yandex.ru>
2026-02-06 16:44:54 +0300

    feat: add button component (App.tsx not modified)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,7 @@
-// src/app/App.tsx
-import React from 'react';
-// Правильный путь - без папки ButtonUI
-import ButtonUI from '../shared/ui/ButtonUI';
-
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div>
       <h1>SkillSwap</h1>
-      
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
-        <ButtonUI variant="primary" title="Войти" />
-        <ButtonUI variant="secondary" title="Регистрация" />
-        <ButtonUI variant="primary" title="Отправить" disabled={true} />
-        <ButtonUI 
-          variant="social" 
-          title="Войти через Google" 
-          iconLeft={<span>G</span>}
-        />
-        <ButtonUI variant="submit" title="Сохранить" />
-      </div>
     </div>
   );
 }
