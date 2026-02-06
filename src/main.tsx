488cea441f4427f38a23b0a781787bf9a7d88207
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-06 14:00:50 +0300

    style(index.css): add styles for body and root

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/global.css';
-import './index.css';
+import './styles/reset.css';
+import './styles/index.css';
 import App from './app/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
