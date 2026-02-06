6ffa44144b5fb054f4b174a999cf37fdb228403f
Alina Ivanova <filledelave15@yandex.ru>
2026-02-06 20:56:48 +0300

    feat: add toggle props and icon to ThemeToggle

--- src/widgets/ThemeToggle/ThemeToggle.tsx
+++ src/widgets/ThemeToggle/ThemeToggle.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './ThemeToggle.module.css';
 import moonIcon from '../../assets/icons/moon.svg';
+import sunIcon from '../../assets/icons/sun.svg';
 
-const ThemeToggle: React.FC = () => {
+interface ThemeToggleProps {
+  isDark: boolean;
+  onToggle?: () => void;
+}
+
+const ThemeToggle: React.FC<ThemeToggleProps> = ({
+  isDark,
+  onToggle,
+}) => {
   return (
     <button
       type="button"
       className={styles.themeToggle}
       aria-label="Сменить тему"
+      onClick={onToggle}
     >
       <img
-        src={moonIcon}
-        alt="theme"
+        src={isDark ? moonIcon : sunIcon}
+        alt={isDark ? 'Тёмная тема' : 'Светлая тема'}
         className={styles.themeToggleIcon}
       />
     </button>
