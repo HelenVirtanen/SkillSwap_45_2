6acb9cc88cac8b2110ad023d11015ae3b61e6ce5
Dima Kotov <DimsiKotov@yandex.ru>
2026-02-07 15:26:45 +0300

    feat(search-input-ui): Сделал все фиксы по ревью SearchInputUI (#20)

--- src/shared/ui/SearchInputUI/SearchInputUI.tsx
+++ src/shared/ui/SearchInputUI/SearchInputUI.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import styles from './SearchInputUI.module.css';
-<<<<<<< HEAD
-=======
-import search from '../../../assets/icons/search.svg';
->>>>>>> d60a549757bdb729e3e240dcf0062c15c293963a
 
 interface SearchInputUIProps {
   placeholder?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-<<<<<<< HEAD
   onClear?: () => void;
-=======
->>>>>>> d60a549757bdb729e3e240dcf0062c15c293963a
 }
 
 const SearchInputUI: React.FunctionComponent<SearchInputUIProps> = ({
   placeholder = 'Искать навык',
   onChange,
-<<<<<<< HEAD
   onClear,
 }) => {
   const [inputValue, setInputValue] = React.useState('');
@@ -54,24 +46,8 @@ const SearchInputUI: React.FunctionComponent<SearchInputUIProps> = ({
           className={styles.clearButton}
         ></button>
       )}
-=======
-}) => {
-  return (
-    <div className={styles.searchInputContainer}>
-      <img src={search} alt="search" className={styles.searchIcon} />
-      <input
-        type="text"
-        placeholder={placeholder}
-        onChange={onChange}
-        className={styles.searchInput}
-      />
->>>>>>> d60a549757bdb729e3e240dcf0062c15c293963a
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default SearchInputUI;
-=======
-export default SearchInputUI;
->>>>>>> d60a549757bdb729e3e240dcf0062c15c293963a
