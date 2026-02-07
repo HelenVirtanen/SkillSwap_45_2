ab30c87e0128122155d1c64bb393cb4f89d7012a
Anna Petrushkina <annaspetrushkina@gmail.com>
2026-02-07 23:16:01 +0300

    feat: added HeaderUserMenu

---
+++ src/shared/ui/HeaderUserMenuUI/HeaderUserMenuUI.tsx
@@ -0,0 +1,45 @@
+import React, { type MouseEventHandler } from 'react';
+import styles from './HeaderUserMenuUI.module.css';
+import NotificationBell from '../../../widgets/NotificationBell/NotificationBell';
+import FavoriteButtonUI from '../FavoriteButtonUI/FavoriteButtonUI';
+
+interface HeaderUserMenuUIProps {
+  hasNewNotifications: boolean;
+  userName: string;
+  userAvatarUrl: string;
+  onNotificationBellClick: MouseEventHandler<HTMLButtonElement>;
+  onFavoriteButtonClick: MouseEventHandler<HTMLButtonElement>;
+  onUserClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const HeaderUserMenuUI: React.FunctionComponent<HeaderUserMenuUIProps> = ({
+  hasNewNotifications,
+  userName,
+  userAvatarUrl,
+  onNotificationBellClick,
+  onFavoriteButtonClick,
+  onUserClick,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.iconsContainer}>
+        <NotificationBell
+          hasNewNotifications={hasNewNotifications}
+          onClick={onNotificationBellClick}
+        />
+        <FavoriteButtonUI onClick={onFavoriteButtonClick} />
+      </div>
+
+      <button
+        className={styles.userContainer}
+        type="button"
+        onClick={onUserClick}
+      >
+        <span className={styles.userName}>{userName}</span>
+        <img src={userAvatarUrl} alt={userName} className={styles.userAvatar} />
+      </button>
+    </div>
+  );
+};
+
+export default HeaderUserMenuUI;
