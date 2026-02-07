c098869d5abf498f82d434369f2c1b6e45382b8d
Deluxo <deluxo31@gmail.com>
2026-02-08 02:34:05 +0300

    fix: remove test code from MainLayout

--- src/app/layout/MainLayout/MainLayout.tsx
+++ src/app/layout/MainLayout/MainLayout.tsx
@@ -11,16 +11,6 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <div style={{width: '324px', padding: '20px', background: '#f0f0f0', margin: '20px 0' }}>
-        <h3>Тестовый компонент:</h3>
-        <UserInfoUI
-          avatar="https://1avatara.ru/pic/men/man0016.jpg"
-          name="Тестовый Пользователь"
-          birthDate="1988-01-01"
-          city="Тестовый Город"
-          isFavorite={false}
-        />
-      </div>
       <main className={styles.main}>{children}</main>
     </div>
   );
