f913493962e66a8cdc5a42215b6dd734aec6e36d
Anna Petrushkina <annaspetrushkina@gmail.com>
2026-02-07 19:42:27 +0300

    refactor: change img to svg component

--- src/shared/ui/FavoriteButtonUI/FavoriteButtonUI.tsx
+++ src/shared/ui/FavoriteButtonUI/FavoriteButtonUI.tsx
@@ -1,7 +1,7 @@
 import React, { type MouseEventHandler } from 'react';
 import styles from './FavoriteButtonUI.module.css';
-import likeIcon from '../../../assets/icons/like.svg';
-import likeSelectedIcon from '../../../assets/icons/like-selected.svg';
+import LikeIcon from '../../../assets/icons/like.svg?react';
+import LikeSelectedIcon from '../../../assets/icons/like-selected.svg?react';
 
 interface FavoriteButtonUIProps {
   isActive: boolean;
@@ -13,12 +13,15 @@ const FavoriteButtonUI: React.FunctionComponent<FavoriteButtonUIProps> = ({
   onClick,
 }) => {
   return (
-    <button type="button" className={styles.button} onClick={onClick}>
-      <img
-        src={isActive ? likeSelectedIcon : likeIcon}
-        alt="Избранное"
-        className={styles.icon}
-      />
+    <button
+      type="button"
+      className={styles.button}
+      onClick={onClick}
+      aria-label={isActive ? 'Удалить из избранного' : 'В избранное'}
+    >
+      isActive
+        ? (<LikeSelectedIcon className={styles.icon} />)
+        : (<LikeIcon className={styles.icon} />)
     </button>
   );
 };
