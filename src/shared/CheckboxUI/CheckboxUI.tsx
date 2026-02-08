45e44649e3c341486d8ddc5c50037495272c6fc8
Abramenko Ekaterina <katiatgn@yandex.ru>
2026-02-08 11:36:22 +0300

    feat: implement and refine CheckboxUI component with custom styles (#25)

--- src/shared/CheckboxUI/CheckboxUI.tsx
+++ src/shared/CheckboxUI/CheckboxUI.tsx
@@ -52,12 +52,59 @@ export const CheckboxUI: FC<CheckboxUIProps> = ({
       />
 
       <span className={styles.icon}>
-        {state === 'default' && <img src={CheckboxDefaultIcon} alt="" />}
-        {state === 'done' && <img src={CheckboxDoneIcon} alt="" />}
-        {state === 'remove' && <img src={CheckboxRemoveIcon} alt="" />}
+        {state === 'default' && <CheckboxDefaultIcon/>}
+        {state === 'done' && <CheckboxDoneIcon />}
+        {state === 'remove' && <CheckboxRemoveIcon/>}
       </span>
 
       <span className={styles.label}>{label}</span>
     </label>
   );
 };
+
+// чтобы проверить работу надо вставить и переменные подключить к main
+// import { useState } from 'react';
+// import { CheckboxUI } from '../../shared/CheckboxUI/CheckboxUI';
+// import type { CheckboxState } from '../../shared/CheckboxUI/CheckboxUI';
+
+// const MainPage: React.FC = () => {
+//   const [categoryState, setCategoryState] =
+//     useState<CheckboxState>('default');
+
+//   const [subCategoryState, setSubCategoryState] =
+//     useState<CheckboxState>('default');
+
+//   const toggleCategory = (current: CheckboxState): CheckboxState => {
+//     return current === 'default' ? 'remove' : 'default';
+//   };
+
+//   const toggleSubCategory = (current: CheckboxState): CheckboxState => {
+//     return current === 'default' ? 'done' : 'default';
+//   };
+
+//   return (
+//     <div style={{ padding: 40 }}>
+//       <h1>Главная страница</h1>
+
+//       <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
+//         <CheckboxUI
+//           label="Категория"
+//           state={categoryState}
+//           onChange={(state) =>
+//             setCategoryState(toggleCategory(state))
+//           }
+//         />
+
+//         <CheckboxUI
+//           label="Подкатегория"
+//           state={subCategoryState}
+//           onChange={(state) =>
+//             setSubCategoryState(toggleSubCategory(state))
+//           }
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MainPage;
