a766219163bb7e2e6a823ba2faeafa788ec88d03
Alina Ivanova <filledelave15@yandex.ru>
2026-02-08 20:13:04 +0300

    feat: update ServerErrorPage with ErrorContent

--- src/pages/ServerErrorPage/ServerErrorPage.tsx
+++ src/pages/ServerErrorPage/ServerErrorPage.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 import ErrorContent from '../../widgets/ErrorContent/ErrorContent';
-import ServerErrorImage from '../../assets/icons/server-error.svg?react';
+import serverErrorIcon from '../../assets/illustrations/error-500.svg';
 import styles from './ServerErrorPage.module.css';
 
-const ServerErrorPage = () => {
-  return (
-    <div className={styles.errorPage500}>
-      <ErrorContent
-        image={<ServerErrorImage />}
-        title="Ошибка сервера"
-        description="Попробуйте обновить страницу позже"
-      />
-    </div>
-  );
-};
+const ServerErrorPage: React.FC = () => (
+  <div className={styles.errorPage500}>
+    <ErrorContent
+      image={<img src={serverErrorIcon} alt="500 Server Error" />}
+      title="На сервере произошла ошибка"
+      description="Попробуйте позже или вернитесь на главную страницу"
+    />
+  </div>
+);
 
 export default ServerErrorPage;
