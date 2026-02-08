e8897b419e917db8741404d34a70c883ba3a636e
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-08 17:50:23 +0300

    fix(ErrorContent.module.css): edit margin-top errorContainer

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import MainLayout from '../../app/layout/MainLayout/MainLayout';
 import ErrorContent from '../../widgets/ErrorContent/ErrorContent';
 import notFoundIcon from "../../assets/illustrations/error-404.svg";
-import styles from './NotFoundPage.module.css';
 
 const NotFoundPage: React.FC = () => (
   <MainLayout>
-    <main className={styles.container}>
       <ErrorContent
         image={<img src={notFoundIcon} alt="404 Not Found" />}
         title="Страница не найдена"
         description="К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже."
       />
-    </main>
   </MainLayout>
 );
 
