0b2d3e364511d80de946cf5c40a15380c85e4e98
Roman Zhohov <zhohov.roman@yandex.ru>
2026-02-08 17:35:07 +0300

    feat: add 404 Not Found page with ErrorContent component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,28 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout/MainLayout';
 import MainPage from '../pages/MainPage/MainPage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
     <Routes>
+      {/* Главная страница */}
       <Route
         path="/"
         element={
           <MainLayout>
             <MainPage />
           </MainLayout>
+        }
+      />
+      
+      {/* Страница 404 - должна быть ПОСЛЕДНЕЙ */}
+      <Route
+        path="*"
+        element={
+          
+            <NotFoundPage />
+          
         }
       />
     </Routes>
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import MainLayout from '../../app/layout/MainLayout/MainLayout';
-import ErrorPageContent from '../../../src/widgets/ErrorPageContent/ErrorPageContent'; // ← ЭТОТ ПУТЬ!
+import ErrorContent from '../../widgets/ErrorContent/ErrorContent';
 import notFoundIcon from "../../assets/illustrations/error-404.svg";
 import styles from './NotFoundPage.module.css';
 
 const NotFoundPage: React.FC = () => (
   <MainLayout>
-    <div className={styles.container}>
-      <ErrorPageContent
+    <main className={styles.container}>
+      <ErrorContent
         image={<img src={notFoundIcon} alt="404 Not Found" />}
         title="Страница не найдена"
         description="К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже."
       />
-    </div>
+    </main>
   </MainLayout>
 );
 
