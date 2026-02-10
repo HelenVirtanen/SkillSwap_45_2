d9641d214ec5b6fa2c964906ba4f1116f6adf707
Dima Kotov <DimsiKotov@yandex.ru>
2026-02-10 16:07:48 +0300

    feat(auto-complete-ui-89)

--- src/app/layout/MainLayout/MainLayout.tsx
+++ src/app/layout/MainLayout/MainLayout.tsx
@@ -2,14 +2,14 @@ import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.css';
 import HeaderMain from '../Header/HeaderMain';
 import Footer from '../Footer/Footer';
-import AutoCompleteUI from '../../../shared/ui/AutoCompleteUI/AutoCompleteUI'
 
 const MainLayout = () => {
   return (
     <div className={styles.wrapper}>
       <HeaderMain />
-      <AutoCompleteUI/>
-      <main className={styles.main}><Outlet/></main>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
