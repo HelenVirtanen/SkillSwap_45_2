efc7cad30c22dda5ea4335fe328f8e6724a0c6da
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-10 11:38:02 +0300

    feat(DropDownUI.tsx): add story to storybook, add line-height

---
+++ src/shared/ui/DropDownUI/DropDown.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import DropDownUI from './DropDownUI';
+
+const meta = {
+  title: 'Main/UI/DropDownUI',
+  component: DropDownUI,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof DropDownUI>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Пол',
+    options: ['Не указан', 'Мужской', 'Женский'],
+  },
+};
+
+export const WithSelectedValue: Story = {
+  args: {
+    title: 'Пол',
+    options: ['Не указан', 'Мужской', 'Женский'],
+    selected: 'Женский',
+  },
+};
