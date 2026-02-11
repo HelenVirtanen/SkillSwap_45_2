a1751b20007ca37f77eea1dd6904968b41539e6e
Anna Mikhaylova <annamispb@gmail.ccom>
2026-02-11 20:01:03 +0300

    adjusted formatting

--- src/shared/ui/ModalWrapperUI/ModalWrapperUI.tsx
+++ src/shared/ui/ModalWrapperUI/ModalWrapperUI.tsx
@@ -1,19 +1,19 @@
-import { ReactNode, MouseEvent } from "react";
-import { createPortal } from "react-dom";
-import styles from "./ModalWrapperUI.module.css";
+import { ReactNode, MouseEvent } from 'react';
+import { createPortal } from 'react-dom';
+import styles from './ModalWrapperUI.module.css';
 
 type ModalWrapperUIProps = {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
-  size?: "md" | "lg"; // md = 556, lg = 1024
+  size?: 'md' | 'lg'; // md = 556, lg = 1024
 };
 
 export const ModalWrapperUI = ({
   isOpen,
   onClose,
   children,
-  size = "md",
+  size = 'md',
 }: ModalWrapperUIProps) => {
   if (!isOpen) return null;
 
@@ -29,13 +29,13 @@ export const ModalWrapperUI = ({
     <div className={styles.overlay} onClick={handleOverlayClick}>
       <div
         className={`${styles.modal} ${
-          size === "lg" ? styles.large : styles.medium
+          size === 'lg' ? styles.large : styles.medium
         }`}
         onClick={handleContentClick}
       >
         {children}
       </div>
     </div>,
-    document.body
+    document.body,
   );
 };
