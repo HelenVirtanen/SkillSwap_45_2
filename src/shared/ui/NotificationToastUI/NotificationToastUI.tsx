518875d472db9cfae9033aa6bfc33347e3592848
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-11 20:31:37 +0300

    fix(NotificationToast): edit styles and add props children, onAction

--- src/shared/ui/NotificationToastUI/NotificationToastUI.tsx
+++ src/shared/ui/NotificationToastUI/NotificationToastUI.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import styles from './NotificationToastUI.module.css';
-import Idea from '@assets/icons/idea.svg?react';
+import IconIdea from '@assets/icons/idea.svg?react';
 import Cross from '@assets/icons/cross.svg?react';
 
-
 interface NotificationToastUIProps {
-    onClose: () => void;
+  onClose: () => void;
+  onAction?: () => void;
+  children: ReactNode;
 }
 
-const NotificationToastUI: React.FC<NotificationToastUIProps> = ({ onClose }) => {
-    const [isHovered, setIsHovered] = useState(false);
+const NotificationToastUI: React.FC<NotificationToastUIProps> = ({
+  onClose,
+  onAction,
+  children,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    return (
-      <div
-        className={styles.notificationToast}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <Idea className={styles.icon} />
-        <span className={styles.text}>{/* Тут нужно будет добавить переменную пользователя */}Олег предлагает вам обмен</span>
-        <button className={styles.closeButton} onClick={onClose}>
-          <Cross/>
+  return (
+    <div
+      className={styles.notificationToast}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <IconIdea className={styles.icon} />
+      <span className={styles.text}>{children}</span>
+      <button className={styles.closeButton} onClick={onClose}>
+        <Cross />
+      </button>
+      {isHovered && (
+        <button className={styles.goButton} onClick={onAction}>
+          Перейти
         </button>
-        {isHovered && (
-          <button className={styles.goButton}>Перейти</button>
-        )}
-      </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default NotificationToastUI;
