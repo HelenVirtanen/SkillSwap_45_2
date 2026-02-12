a1188fcffa4712c63bd095448d0a496821da3855
Dima Kotov <DimsiKotov@yandex.ru>
2026-02-12 17:53:40 +0300

    feat: notifications-panel-101

--- src/app/layout/MainLayout/MainLayout.tsx
+++ src/app/layout/MainLayout/MainLayout.tsx
@@ -2,16 +2,15 @@ import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.css';
 import HeaderMain from '../Header/HeaderMain';
 import Footer from '../Footer/Footer';
-import NotificationsPanel from '@widgets/NotificationsPanel/NotificationsPanel'
-
 
 const MainLayout = () => {
   return (
     <div className={styles.wrapper}>
       <HeaderMain />
-      <NotificationsPanel/>
-      {/* <main className={styles.main}><Outlet/></main> */}
-      {/* <Footer /> */}
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
