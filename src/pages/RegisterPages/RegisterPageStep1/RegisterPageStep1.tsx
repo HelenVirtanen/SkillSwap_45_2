8c0b1d3ce68ea1be156b13486724737d316aa2dc
Alexander Grankin <sacha.grankin.04@mail.ru>
2026-02-14 01:13:06 +0300

    feat: added styles

--- src/pages/RegisterPages/RegisterPageStep1/RegisterPageStep1.tsx
+++ src/pages/RegisterPages/RegisterPageStep1/RegisterPageStep1.tsx
@@ -5,11 +5,41 @@ import appleLogo from "../../../assets/icons/logo/apple-logo.svg";
 import lightBulb from "../../../assets/illustrations/light-bulb.svg";
 import InputUI from "@shared/ui/InputUI/InputUI";
 import Stepper from "@widgets/Stepper/Stepper";
+import { useState } from "react";
 
 const RegisterPageStep1: React.FC = () => {
+    const [password, setPassword] = useState('');
+    const [passwordHelperText, setPasswordHelperText] = useState<string | undefined>(undefined);
+
+    // const [email, setEmail] = useState('');
+    // const [emailErrorText, setEmailErrorText] = useState<string | undefined>(undefined);
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+      e.preventDefault();
+      if (password.length < 8) {
+        setPasswordHelperText('Пароль должен содержать не менее 8 знаков');
+      } else {
+        setPasswordHelperText('Надёжный');
+      }
+    }
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const newPassword = e.target.value;
+      setPassword(e.target.value);
+        
+      if (newPassword.length >= 8) {
+        setPasswordHelperText('Надёжный');
+      } else {
+        setPasswordHelperText(undefined);
+      }
+    };
+
+    // const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    //   setEmail(e.target.value);
+    // }
 
     return (
-      <>
+      <div className={styles.container}>
         <Stepper/>
         <div className={styles.content} >
           <div className={styles.register}>
@@ -26,26 +56,31 @@ const RegisterPageStep1: React.FC = () => {
               />
             </div>
             <div className={styles.divider}>
-              <span className={styles.divider__text}>или</span>
+              <span>или</span>
             </div>
-            <form action="">
-              <InputUI 
-                label="Email" 
-                onChange={() => {}} 
-                placeholder="Введите email"
-                type="email"
-                error="Email уже используется"
-              />
-              <InputUI 
-                label="Пароль" 
-                onChange={() => {}} 
-                placeholder="Придумайте надёжный пароль" 
-                type="password"
-                error="Пароль должен содержать не менее 8 знаков"
-              />
+            <form action="" onSubmit={handleSubmit} className={styles.registerForm}>
+              <div className={styles.registerForm__inputContainer}>
+                <InputUI 
+                  label="Email" 
+                  // onChange={handleEmailChange} 
+                  onChange={() => {}}
+                  placeholder="Введите email"
+                  type="email"
+                  // error="Email уже используется"
+                />
+                <InputUI 
+                  label="Пароль" 
+                  onChange={handlePasswordChange}
+                  placeholder="Придумайте надёжный пароль" 
+                  type="password"
+                  helperText={passwordHelperText}
+                />
+              </div>
               <ButtonUI
                 variant="submit"
                 title="Далее"
+                type="submit"
+                className={styles.registerForm__submit}
               />
             </form>
           </div>
@@ -57,7 +92,7 @@ const RegisterPageStep1: React.FC = () => {
             </div>
           </div>
         </div>
-      </>
+      </div>
     )
 }
 
