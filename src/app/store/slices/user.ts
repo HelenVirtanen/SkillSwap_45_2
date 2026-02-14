c09eb1d1a741e70c23ec8ea673d8105061d864e1
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-14 21:59:46 +0300

    feat(store.ts): add redux, rootReducer, store, userSlice

---
+++ src/app/store/slices/user.ts
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { getUsers } from '@api/users';
+import { RootState } from '../store';
+
+export type TUser = {
+  id: number;
+  name: string;
+  email: string;
+  city?: string;
+};
+
+interface UsersState {
+  users: TUser[];
+  status: 'idle' | 'loading' | 'failed';
+  error: string | null;
+}
+
+const initialState: UsersState = {
+  users: [],
+  status: 'idle',
+  error: null,
+};
+
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const res = await getUsers();
+  return res;
+});
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    clearUsers(state) {
+      state.users = [];
+      state.status = 'idle';
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action: PayloadAction<TUser[]>) => {
+        state.status = 'idle';
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Ошибка при загрузке пользователей';
+      });
+  },
+});
+
+export const selectUsers = (state: RootState ) => state.user.users;
+export const selectUsersStatus = (state: RootState ) => state.user.status;
+export const selectUsersError = (state: RootState ) => state.user.error;
+
+export const { clearUsers } = userSlice.actions;
+export default userSlice.reducer;
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import store from './app/store/store.ts';
+import { Provider } from 'react-redux';
+
 import App from './app/App.tsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
