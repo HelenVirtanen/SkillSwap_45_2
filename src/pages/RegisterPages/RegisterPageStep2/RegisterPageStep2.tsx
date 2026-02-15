7a9edc29a2efdfbdaa65d08a511a647d003c7945
Dima Kotov <DimsiKotov@yandex.ru>
2026-02-15 21:05:43 +0300

    feat: feature/register-page-step-2-135

--- src/pages/RegisterPages/RegisterPageStep2/RegisterPageStep2.tsx
+++ src/pages/RegisterPages/RegisterPageStep2/RegisterPageStep2.tsx
@@ -1,19 +1,23 @@
-import Step2ProfileInfo, { Step2Data } from "@features/forms/RegisterSteps/Step2ProfileInfo/Step2ProfileInfo";
-import Stepper from "@widgets/Stepper/Stepper";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import Step2ProfileInfo, {
+  Step2Data,
+} from '@features/forms/RegisterSteps/Step2ProfileInfo/Step2ProfileInfo';
+import Stepper from '@widgets/Stepper/Stepper';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './RegisterPageStep2.module.css';
+import UserInfo from '@assets/illustrations/user-info.svg?react';
+import { IllustrationPanel } from '@widgets/IllustrationPanel/IllustrationPanel';
 
 const RegisterPageStep2: React.FC = () => {
   const navigate = useNavigate();
 
   const handleBack = () => {
     navigate('/register/step1');
-  }
+  };
 
   const handleNext = () => {
-    navigate('/register/step3')
-  }
-
+    navigate('/register/step3');
+  };
 
   const [formData, setFormData] = useState<Step2Data>({
     firstName: '',
@@ -21,32 +25,43 @@ const RegisterPageStep2: React.FC = () => {
     gender: undefined,
     city: '',
     categorySkill: [],
-    subcategorySkill: []    
+    subcategorySkill: [],
   });
 
   const handleStep2Change = <K extends keyof Step2Data>(
-    field: K, 
-    value: Step2Data[K]
+    field: K,
+    value: Step2Data[K],
   ) => {
-    setFormData(prev => ({
+    setFormData((prev) => ({
       ...prev,
-      [field]: value
+      [field]: value,
     }));
   };
 
-  console.log(formData)
+  console.log(formData);
   return (
-    <div>
-      <Stepper currentStep={2}/>
-      <Step2ProfileInfo 
-        initialData={formData} 
-        onFieldChange={handleStep2Change}
-        handleBack={handleBack}
-        handleNext={handleNext}
-      />
+    <div className={styles.mainContainer}>
+      <div className={styles.stepper}>
+      <Stepper currentStep={2} />
+      </div>
+      <div className={styles.mainWrapper}>
+        <Step2ProfileInfo
+          initialData={formData}
+          onFieldChange={handleStep2Change}
+          handleBack={handleBack}
+          handleNext={handleNext}
+        />
+
+        <div>
+          <IllustrationPanel
+            image={<UserInfo />}
+            title="Расскажите немного о себе"
+            description="Это поможет другим людям лучше вас узнать, чтобы выбрать для обмена"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default RegisterPageStep2;
-
