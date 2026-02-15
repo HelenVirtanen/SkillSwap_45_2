d7b2f5d0969ec511966ee80cba17109b64727e28
Abramenko Ekaterina <katiatgn@yandex.ru>
2026-02-15 11:36:58 +0300

    feat: implement UsersCatalog component with real API data fetching (#номер_задачи)

--- src/widgets/UsersCatalog/UsersCatalog.stories.tsx
+++ src/widgets/UsersCatalog/UsersCatalog.stories.tsx
@@ -1,34 +1,34 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { UsersCatalog } from './UsersCatalog';
+// import type { Meta, StoryObj } from '@storybook/react';
+// import { UsersCatalog } from './UsersCatalog';
 
-const meta: Meta<typeof UsersCatalog> = {
-  title: 'Main/Widgets/UsersCatalog',
-  component: UsersCatalog,
-  parameters: {
-    layout: 'fullscreen',
-  },
-};
+// const meta: Meta<typeof UsersCatalog> = {
+//   title: 'Main/Widgets/UsersCatalog',
+//   component: UsersCatalog,
+//   parameters: {
+//     layout: 'fullscreen',
+//   },
+// };
 
-export default meta;
+// export default meta;
 
-type Story = StoryObj<typeof UsersCatalog>;
+// type Story = StoryObj<typeof UsersCatalog>;
 
 
 
-export const Default: Story = {
-  render: () => <UsersCatalog />,
-};
+// export const Default: Story = {
+//   render: () => <UsersCatalog />,
+// };
 
-/* ---------------- ТОЛЬКО 1 СЕКЦИЯ ---------------- */
+// /* ---------------- ТОЛЬКО 1 СЕКЦИЯ ---------------- */
 
-export const OnlyPopular: Story = {
-  render: () => (
-    <main style={{ width: 1020, margin: '0 auto' }}>
-      <section style={{ marginBottom: 40 }}>
-        <UsersCatalog />
-      </section>
-    </main>
-  ),
-};
+// export const OnlyPopular: Story = {
+//   render: () => (
+//     <main style={{ width: 1020, margin: '0 auto' }}>
+//       <section style={{ marginBottom: 40 }}>
+//         <UsersCatalog />
+//       </section>
+//     </main>
+//   ),
+// };
 
 
