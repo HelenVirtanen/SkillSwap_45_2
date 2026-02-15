7811147922466f59914ac7835dea8b9edd1c9ddb
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-15 17:13:40 +0300

    fix(Step2ProfileInfo.tsx): add AvatarUI and edit styles

--- src/features/forms/RegisterSteps/Step2ProfileInfo/Step2ProfileInfo.tsx
+++ src/features/forms/RegisterSteps/Step2ProfileInfo/Step2ProfileInfo.tsx
@@ -1,120 +1,120 @@
-import InputUI from "@shared/ui/InputUI/InputUI"
-import styles from "./Step2ProfileInfo.module.css"
-import DropDownUI from "@shared/ui/DropDownUI/DropDownUI"
-import { MultiSelectDropdownUI } from "@shared/ui/MultiSelectDropdownUI/MultiSelectDropdownUI"
-import { useState } from "react"
-import ButtonUI from "@shared/ui/ButtonUI/ButtonUI"
-import AutoCompleteUI from "@shared/ui/AutoCompleteUI/AutoCompleteUI"
-import DatePicker from "@widgets/DatePicker/DatePicker"
-
+import InputUI from '@shared/ui/InputUI/InputUI';
+import styles from './Step2ProfileInfo.module.css';
+import DropDownUI from '@shared/ui/DropDownUI/DropDownUI';
+import { MultiSelectDropdownUI } from '@shared/ui/MultiSelectDropdownUI/MultiSelectDropdownUI';
+import { useState } from 'react';
+import ButtonUI from '@shared/ui/ButtonUI/ButtonUI';
+import AutoCompleteUI from '@shared/ui/AutoCompleteUI/AutoCompleteUI';
+import DatePicker from '@widgets/DatePicker/DatePicker';
+import AvatarUI from '@shared/ui/AvatarUI/AvatarUI';
 
 //
 //
-//3) Добавить datapicker потом 
+//3) Добавить datapicker потом
 //4) Убрать фейковые данные
 //5) Подправить как для навыков DropDown
 //6) Связать категории подкатегории (аля функция и на входи название приходит(категории или подкатегории)
 // и значит пересчитываем выдаваемые значения для второго списка
 //7) Сделать input для городов на основе моего DropDown
-//8) 
+//8)
 //9) Ещё надо сделать label + input скорее всего но я хз
-const optionsGenders = ['Не указан','Мужской', 'Женский'] //Для пола
+const optionsGenders = ['Не указан', 'Мужской', 'Женский']; //Для пола
 const optionsCategorySkillsWantedToLearn = [
   {
-    "id": 1,
-    "label": "Бизнес и карьера",
-    "value": "бизнес и карьера"
+    id: 1,
+    label: 'Бизнес и карьера',
+    value: 'бизнес и карьера',
   },
   {
-    "id": 2,
-    "label": "Творчество и искусство",
-    "value": "творчество и искусство"
+    id: 2,
+    label: 'Творчество и искусство',
+    value: 'творчество и искусство',
   },
   {
-    "id": 3,
-    "label": "Иностранные языки",
-    "value": "иностранные языки"
+    id: 3,
+    label: 'Иностранные языки',
+    value: 'иностранные языки',
   },
   {
-    "id": 4,
-    "label": "Здоровье и лайфстайл",
-    "value": "здоровье и лайфстайл"
+    id: 4,
+    label: 'Здоровье и лайфстайл',
+    value: 'здоровье и лайфстайл',
   },
   {
-    "id": 5,
-    "label": "Дом и уют",
-    "value": "дом и уют"
-  }
-]
+    id: 5,
+    label: 'Дом и уют',
+    value: 'дом и уют',
+  },
+];
 const optionsSubCategorySkillsWantedToLearn = [
   {
-    "id": 1,
-    "label": "Рисование и иллюстрация",
-    "value": "Рисование и иллюстрация"
+    id: 1,
+    label: 'Рисование и иллюстрация',
+    value: 'Рисование и иллюстрация',
   },
   {
-    "id": 2,
-    "label": "Фотография",
-    "value": "Фотография"
+    id: 2,
+    label: 'Фотография',
+    value: 'Фотография',
   },
   {
-    "id": 3,
-    "label": "Видеомонтаж",
-    "value": "Видеомонтаж"
+    id: 3,
+    label: 'Видеомонтаж',
+    value: 'Видеомонтаж',
   },
   {
-    "id": 4,
-    "label": "Музыка и звук",
-    "value": "Музыка и звук"
+    id: 4,
+    label: 'Музыка и звук',
+    value: 'Музыка и звук',
   },
   {
-    "id": 5,
-    "label": "Актёрское мастерство",
-    "value": "Актёрское мастерство"
+    id: 5,
+    label: 'Актёрское мастерство',
+    value: 'Актёрское мастерство',
   },
   {
-    "id": 6,
-    "label": "Креативное письмо",
-    "value": "Креативное письмо"
+    id: 6,
+    label: 'Креативное письмо',
+    value: 'Креативное письмо',
   },
   {
-    "id": 7,
-    "label": "Арт-терапия",
-    "value": "Арт-терапия"
+    id: 7,
+    label: 'Арт-терапия',
+    value: 'Арт-терапия',
   },
   {
-    "id": 8,
-    "label": "Декор и DIY",
-    "value": "Декор и DIY"
-  }
-]
+    id: 8,
+    label: 'Декор и DIY',
+    value: 'Декор и DIY',
+  },
+];
 const optionsCity = [
   {
-    "id": 1,
-    "label": "Москва",
-    "value": "Москва"
+    id: 1,
+    label: 'Москва',
+    value: 'Москва',
   },
   {
-    "id": 2,
-    "label": "Новосибирск",
-    "value": "Новосибирск"
+    id: 2,
+    label: 'Новосибирск',
+    value: 'Новосибирск',
   },
   {
-    "id": 3,
-    "label": "Томск",
-    "value": "Томск"
+    id: 3,
+    label: 'Томск',
+    value: 'Томск',
   },
   {
-    "id": 4,
-    "label": "Париж",
-    "value": "Париж"
+    id: 4,
+    label: 'Париж',
+    value: 'Париж',
   },
   {
-    "id": 6,
-    "label": "Пекин",
-    "value": "Пекин"
+    id: 6,
+    label: 'Пекин',
+    value: 'Пекин',
   },
-]
+];
 //Убрать ширину form так как вдруг расстягиваться будет.
 
 export type Step2Data = {
@@ -124,24 +124,21 @@ export type Step2Data = {
   city?: string;
   categorySkill?: string[];
   subcategorySkill?: string[];
-}
-
-
-
+};
 
 type Step2ProfileInfoProps = {
-  initialData?: Step2Data,
-  onChangeOfWantedSkills?:() => void,
-  onFieldChange: <K extends keyof Step2Data>(field: K, value: Step2Data[K]) => void;
+  initialData?: Step2Data;
+  onChangeOfWantedSkills?: () => void;
+  onFieldChange: <K extends keyof Step2Data>(
+    field: K,
+    value: Step2Data[K],
+  ) => void;
   handleBack: () => void;
   handleNext: () => void;
-}
+};
 
-
-
-const Step2ProfileInfo = (props:Step2ProfileInfoProps) => {
-
-  const {initialData,onFieldChange,handleBack,handleNext} = props
+const Step2ProfileInfo = (props: Step2ProfileInfoProps) => {
+  const { initialData, onFieldChange, handleBack, handleNext } = props;
 
   const [localData, setLocalData] = useState<Step2Data>(() => ({
     firstName: initialData?.firstName || '',
@@ -149,47 +146,70 @@ const Step2ProfileInfo = (props:Step2ProfileInfoProps) => {
     gender: initialData?.gender,
     city: initialData?.city || '',
     categorySkill: initialData?.categorySkill || [],
-    subcategorySkill: initialData?.subcategorySkill || []
+    subcategorySkill: initialData?.subcategorySkill || [],
   }));
 
   const handleFieldChange = <K extends keyof Step2Data>(
-    field: K, 
-    value: Step2Data[K]
+    field: K,
+    value: Step2Data[K],
   ) => {
-    setLocalData(prev => ({ ...prev, [field]: value }));
-    onFieldChange(field, value); 
+    setLocalData((prev) => ({ ...prev, [field]: value }));
+    onFieldChange(field, value);
   };
 
   return (
     <div className={styles.form}>
-      <InputUI value={localData.firstName} label={"Имя"} onChange={(e)=>{handleFieldChange('firstName',e.target.value) }} placeholder="Введите ваше имя"/>
+      <div className={styles.avatarWrapper}>
+        <AvatarUI className="avatar" />
+      </div>
+      <InputUI
+        value={localData.firstName}
+        label={'Имя'}
+        onChange={(e) => {
+          handleFieldChange('firstName', e.target.value);
+        }}
+        placeholder="Введите ваше имя"
+      />
 
       <div className={styles.inLineDataAndGender}>
-        <DatePicker title="Дата рождения" placeholder="дд.мм.гггг" ></DatePicker>
-        <DropDownUI title="Пол" options={optionsGenders} value={localData.gender} onChange={(value)=>handleFieldChange('gender',value)}/>
+        <DatePicker title="Дата рождения" placeholder="дд.мм.гггг"></DatePicker>
+        <DropDownUI
+          title="Пол"
+          options={optionsGenders}
+          value={localData.gender}
+          onChange={(value) => handleFieldChange('gender', value)}
+        />
       </div>
 
-      <AutoCompleteUI onCitySelect={(value)=>handleFieldChange('city',value)}/>
+      <AutoCompleteUI
+        onCitySelect={(value) => handleFieldChange('city', value)}
+      />
 
-      <MultiSelectDropdownUI placeholder='Выберите категорию' label='Категория навыка, которому хотите научиться'
-      options={optionsCategorySkillsWantedToLearn} selected={localData.categorySkill ? localData.categorySkill : []} onChange={(value:string[])=>{handleFieldChange('categorySkill',value)}} />
+      <MultiSelectDropdownUI
+        placeholder="Выберите категорию"
+        label="Категория навыка, которому хотите научиться"
+        options={optionsCategorySkillsWantedToLearn}
+        selected={localData.categorySkill ? localData.categorySkill : []}
+        onChange={(value: string[]) => {
+          handleFieldChange('categorySkill', value);
+        }}
+      />
 
-      <MultiSelectDropdownUI placeholder='Выберите подкатегорию' label='Подкатегория навыка, которому хотите научиться'
-      options={optionsSubCategorySkillsWantedToLearn} selected={localData.subcategorySkill ? localData.subcategorySkill : []} onChange={(value:string[])=>{handleFieldChange('subcategorySkill',value)}}/>
+      <MultiSelectDropdownUI
+        placeholder="Выберите подкатегорию"
+        label="Подкатегория навыка, которому хотите научиться"
+        options={optionsSubCategorySkillsWantedToLearn}
+        selected={localData.subcategorySkill ? localData.subcategorySkill : []}
+        onChange={(value: string[]) => {
+          handleFieldChange('subcategorySkill', value);
+        }}
+      />
 
       <div className={styles.inLineButtons}>
-        <ButtonUI 
-          variant="secondary"
-          title="Назад"
-          onClick={handleBack}
-        />
-        <ButtonUI 
-          variant="primary"
-          title="Продолжить"
-          onClick={handleNext}
-        />
+        <ButtonUI variant="secondary" title="Назад" onClick={handleBack} />
+        <ButtonUI variant="primary" title="Продолжить" onClick={handleNext} />
       </div>
     </div>
-  )
-}
-export default Step2ProfileInfo
+  );
+};
+export default Step2ProfileInfo;
