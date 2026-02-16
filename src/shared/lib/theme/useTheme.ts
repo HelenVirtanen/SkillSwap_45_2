43ca50266d6b737d78b87acf09e94a112376a144
Helen Virtanen <helenvirtanen28@gmail.com>
2026-02-16 23:11:46 +0300

    chore(useTheme.ts): remove console.log

--- src/shared/lib/theme/useTheme.ts
+++ src/shared/lib/theme/useTheme.ts
@@ -3,7 +3,6 @@ import { ThemeContext } from './theme-context';
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
-  console.log('смена темы')
 
   if (!context) {
     throw new Error('useTheme must be used inside ThemeProvider');
